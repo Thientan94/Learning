ea63e3a0caf39bc4521cfd9e33dfc31b05453b04
Thientan94 <phanqthientan@gmail.com>
2022-12-07 17:02:59 +0700

    Fix bai 4

--- Bai4.js
+++ Bai4.js
@@ -1,49 +1,44 @@
 	// 1.4: Giải phương trình bậc 2
 
-function equation2( a,b,c ){
+// Calculate equation1
 
+const equation1 = ( b, c ) => x = ( - c / b ) ;
+const deltaequal0 = ( a, b ) => x = (-b / 2 * a) ;
+//Calculate delta
+
+//Delta = 0
+// Delta > 0
+
+//Calculate Equation2
+function Equation2( a, b, c ){
     let x, x1 = 0;
-
-    if( typeof( a ) === "number" && typeof( b ) === "number" && typeof( c ) === "number" && !isNaN( a ) && !isNaN( b ) && !isNaN( c )){
-
-        if( a == 0 ){
-
-            x = - c / b ;
-            console.log( " Nghiem cua phuong trinh :",x );
-
-        }
-        else{
-
-            const delta = b * b - 4 * a * c;
-
-            if( delta == 0 ){
-
-                x = - b / ( 2 * a );
-                console.log( " Phuong trinh co nghiem kep: x = x1 = ", x );
-
-            }else if( delta < 0 ){
-
-                console.log( " Phuong trinh vo nghiem " );
-
-            }
-
-            else{
-
-                x = ( -b + Math.sqrt( delta ) ) / ( 2 * a);
-                x1 = ( -b - Math.sqrt( delta ) ) / ( 2 * a);
-
-                console.log( " Phuong trinh co 2 nghiem rieng biet : x = " , x, "x1 = ", x1 );
-
-            }
-
-        }
-
-    }else{
-        console.log(" a, b ,c must be number");
+    if( typeof( a ) !== "number" || isNaN( a ) || typeof( b ) !== "number" || isNaN( b ) ||  typeof( c ) !== "number" || isNaN( c )){
+        throw new Error(" a, b, c must be Number");
     }
+    
+    if( a == 0 ) {
+        console.log("Nghiem cua pt la: x =");
+        return equation1( b, c );
+    }
+
+    const delta = b * b - 4 * a * c;
+    
+    if( delta == 0 ) {
+        console.log("Phuong trinh co nghiem kep: x=x1= ");
+        return deltaequal0( a, b );
+    }
+    if( delta < 0 ) {
+        console.log( " Phuong trinh vo nghiem ");
+        return;
+    }
+
+    x = ( -b + Math.sqrt( delta) )/ ( 2 * a ) ;
+    x1 = ( -b - Math.sqrt( delta ) ) / ( 2 * a );
+    console.log("Phuong trinh co 2 nghiem la: x=", x, "x1 =", x1);
     return x, x1;
+
 }
 
-const result = equation2(-1,0,4);
+const result = Equation2( 1, 2, 1 );
 
 console.log(result);
