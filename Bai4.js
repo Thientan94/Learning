5e43871735a065a76f4819b5d98efea572be6251
Thientan94 <phanqthientan@gmail.com>
2022-12-07 09:53:31 +0700

    New Fix

--- Bai4.js
+++ Bai4.js
@@ -1,37 +1,30 @@
 	// 1.4: Giải phương trình bậc 2
 
-function equation2(a,b,c){
+function equation2( a,b,c ){
 
-    let x = 0;
-    let x1 = 0 ;
+    let x, x1 = 0;
 
-    if( isNaN( a ) || isNaN( b ) || isNaN( c ) ){
-        throw new Error(" Nunber cannot be NaN");
-    }else{
+    if( typeof( a ) === "number" && typeof( b ) === "number" && typeof( c ) === "number" && !isNaN( a ) && !isNaN( b ) && !isNaN( c )){
 
-        if( a === 0 ){
-            x = -c/b;
-            console.log( "Nghiem cua phuong trinh :", x );
-        }else
+        if( a == 0 ){
 
-        if( b === 0 ){
-            x = Math.sqrt(-c / a);
+            x = - c / b ;
+            console.log( " Nghiem cua phuong trinh :",x );
+
         }
-
         else{
 
-            const delta = b*b - 4 * a * c;
+            const delta = b * b - 4 * a * c;
 
-            if( delta === 0 ){
+            if( delta == 0 ){
 
-                x = -b / ( 2 * a );
-
+                x = - b / ( 2 * a );
                 console.log( " Phuong trinh co nghiem kep: x = x1 = ", x );
 
-            }else 
+            }else if( delta < 0 ){
 
-            if( delta < 0 ){
                 console.log( " Phuong trinh vo nghiem " );
+
             }
 
             else{
@@ -39,14 +32,18 @@ function equation2(a,b,c){
                 x = ( -b + Math.sqrt( delta ) ) / ( 2 * a);
                 x1 = ( -b - Math.sqrt( delta ) ) / ( 2 * a);
 
-                console.log( " Phuong trinh co 2 nghiem rieng biet : x = ", x, "x1 = " );
+                console.log( " Phuong trinh co 2 nghiem rieng biet : x = " , x, "x1 = ", x1 );
+
             }
 
         }
-        return x,x1;
+
+    }else{
+        console.log(" a, b ,c must be number");
     }
+    return x, x1;
 }
 
-const result = equation2("1",2,1);
+const result = equation2(-1,0,4);
 
 console.log(result);
